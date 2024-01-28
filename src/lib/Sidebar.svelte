618a45c0cba83e4f0ef84a7a2e9e38da5c28f1ae
shawn edgell <shawn_edgell@hotmail.com>
2024-01-27 22:16:33 -0500

    added all mods and pages

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -17,10 +17,122 @@
         route: '/FroMod'
       },
       {
+        name: 'XL Graphics',
+        subcategories: ['Basic', 'Presets', 'Camera'],
+        route: '/XLGraphics'
+      },
+      {
+        name: 'Deck FX',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/DeckFX'
+      },
+      {
+        name: 'Sound Mod',
+        subcategories: ['S',],
+        route: '/SoundMod'
+      },
+      {
         name: 'Boned Ollie Mod',
         subcategories: ['Page 1', 'Page 2'],
         route: '/BonedOllieMod'
-      }
+      },
+      {
+        name: 'XL Gear Mod',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/XLGearMod'
+      },
+/*
+      {
+        name: 'WalkingMod',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/WalkingMod'
+      },
+      {
+        name: 'XL Menu Mod',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/XLMenuMod'
+      },
+      {
+        name: 'Multiplayer++',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/Multiplayer++'
+      },
+      {
+        name: 'Better Replay Mod',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/BetterReplayMod'
+      },
+      {
+        name: 'Grind Tools',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/GrindTools'
+      },
+      {
+        name: 'Lateflip Mod',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/LateflipMod'
+      },
+      {
+        name: 'XL Object Dropper',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/XLObjectDropper'
+      },
+      {
+        name: 'Better Object Dropper',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/BetterObjectDropper'
+      },
+      {
+        name: 'DecalFix',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/DecalFix'
+      },
+      {
+        name: 'XL Random Map',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/XLRandomMap'
+      },
+      {
+        name: 'Break FX',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/BreakFX'
+      },
+      {
+        name: 'Gear Utilities',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/GearUtilities'
+      },
+      {
+        name: 'Exposure Controller',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/ExposureController'
+      },
+      {
+        name: 'Map Editor',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/MapEditor'
+      },
+      {
+        name: 'Grab Customizer',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/GrabCustomizer'
+      },
+      {
+        name: 'Controls Editor',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/ControlsEditor'
+      },
+      {
+        name: 'Not My Feet',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/NotMyFeet'
+      },
+      {
+        name: 'Bill \'O Rumble',
+        subcategories: ['Page 1', 'Page 2'],
+        route: '/BillORumble'
+      },
+*/
     ];
   
     async function handleCategoryClick(event, categoryName) {
