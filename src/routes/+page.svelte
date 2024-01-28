6f4886b12616b81f98920018f4c91893406a06d9
shawn edgell <shawn_edgell@hotmail.com>
2024-01-28 15:39:10 -0500

    getting started section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,8 +24,39 @@
 
   <h2>Changing Skater XL Versions</h2>
   <p>
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    You can easily change versions of Skater XL by following the instructions below.
   </p>
+
+  <h3>Step 1</h3>
+  <p>
+    Right-click Skater XL in your Steam library and select Properties.
+  </p>
+  <div class="max-w-4xl mx-auto py-6">
+    <!-- Your content here -->
+  
+    <img src="XLDowngrade1.png" alt="Descriptive Alt Text" />
+  
+    <!-- More content -->
+  </div>
+
+  <h3>Step 2</h3>
+  <p>
+    Select the Betas tab and choose the version you want to play from the drop-down menu.
+  </p>
+  <ul>
+    <li>None = Skater XL v1.2.7.8 (Public)</li>
+    <li>Alpha = Skater XL v1.2.2.8 (Alpha)
+      <i>- Recommended version</i>
+    </li>
+    <li>Beta = Outdated, please switch to either None or Alpha.</li>
+  </ul>
+  <div class="max-w-4xl mx-auto py-6">
+    <!-- Your content here -->
+  
+    <img src="XLDowngrade2.png" alt="Descriptive Alt Text" />
+  
+    <!-- More content -->
+  </div>
 
   <h2>Download + Install Mods</h2>
   <p>
