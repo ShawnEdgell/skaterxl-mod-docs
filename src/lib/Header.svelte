62b23bd451c01d45081e3f0f8fc708e8040a0c3a
shawn edgell <shawn_edgell@hotmail.com>
2024-01-28 20:04:23 -0500

    added home page, map and gear page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,14 @@
 <header class="text-white text-center space-x-6 bg-blue-500 p-3">
     <a href="/" sveltekit-prefetch class="font-bold text-white hover:text-gray-300">
+      Home
+    </a>
+
+    <a href="/GettingStarted" sveltekit-prefetch class="font-bold text-white hover:text-gray-300">
       Getting Started
+    </a>
+
+    <a href="/Maps&Gear" sveltekit-prefetch class="font-bold text-white hover:text-gray-300">
+      Maps & Gear
     </a>
 
     <a href="/Links" sveltekit-prefetch class="font-bold text-white hover:text-gray-300">
