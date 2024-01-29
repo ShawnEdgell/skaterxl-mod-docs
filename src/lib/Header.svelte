505bb07b199d60adffa3d40b8eabc0ffe2c4a9c5
shawn edgell <shawn_edgell@hotmail.com>
2024-01-29 02:14:02 -0500

    styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,5 @@
-<header class="relative sticky top-0 z-50 bg-blue-500 p-4 flex items-center justify-center">
-  <span class="absolute ml-2 left-4 top-1/2 transform -translate-y-1/2 text-white opacity-75">pre-pre-alpha</span>
+<header class="text-lg relative sticky top-0 z-50 bg-blue-500 p-4 flex items-center justify-center">
+  <span class="text-lg absolute ml-2 left-4 top-1/2 transform -translate-y-1/2 text-white opacity-75">pre-pre-alpha</span>
   
   <nav class="flex justify-center">
     <a href="/" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-4">
@@ -17,7 +17,7 @@
   </nav>
 
   <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center">
-    <span class="text-white mr-2">LIVE</span>
+    <span class="text-white text-lg mr-2">LIVE</span>
     <span class="mr-2 h-2.5 w-2.5 bg-red-500 rounded-full"></span>
   </a>
 </header>
