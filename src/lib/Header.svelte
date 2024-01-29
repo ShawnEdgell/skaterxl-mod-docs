5d8f33b26851ca97fded47ebaa6d94546ccfa341
shawn edgell <shawn_edgell@hotmail.com>
2024-01-29 02:01:07 -0500

    fixed header styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,21 +1,23 @@
-<header class="sticky top-0 z-50 bg-blue-500 p-3 flex justify-between items-center">
-  <span class="text-xs text-white opacity-75 ml-3">pre-pre-alpha</span>
-  <nav class="flex-grow text-center mx-6">
-    <a href="/" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2">
+<header class="relative sticky top-0 z-50 bg-blue-500 p-4 flex items-center justify-center">
+  <span class="absolute ml-2 left-4 top-1/2 transform -translate-y-1/2 text-xs text-white opacity-75">pre-pre-alpha</span>
+  
+  <nav class="flex justify-center">
+    <a href="/" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-4">
       Home
     </a>
-    <a href="/GettingStarted" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2">
+    <a href="/GettingStarted" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-4">
       Getting Started
     </a>
-    <a href="/Maps&Gear" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2">
+    <a href="/Maps&Gear" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-4">
       Maps & Gear
     </a>
-    <a href="/Links" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2">
+    <a href="/Links" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-4">
       Links
     </a>
   </nav>
-  <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="flex items-center">
-    <span class=" text-white mr-2">LIVE</span>
-    <span class="h-2.5 w-2.5 bg-red-500 rounded-full"></span>
+
+  <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center">
+    <span class="text-white mr-2">LIVE</span>
+    <span class="mr-2 h-2.5 w-2.5 bg-red-500 rounded-full"></span>
   </a>
 </header>
