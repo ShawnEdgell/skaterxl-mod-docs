ffa2cdfc6233b7ff89ea4c501637bb169f8b8810
shawn edgell <shawn_edgell@hotmail.com>
2024-01-30 16:49:47 -0500

    header styling fix

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -11,7 +11,7 @@
   </nav>
 
   <!-- Twitch Link -->
-  <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center hidden md:block">
+  <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="absolute right-4 top-1/2 transform -translate-y-1/2 items-center hidden md:block">
     <img src="/Twitch.svg" alt="Twitch" class="h-5 w-5 mr-2">
   </a>
 </header>
