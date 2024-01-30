a5dba44b53313e18ae55d169e4f5aa92bf366ca1
shawn edgell <shawn_edgell@hotmail.com>
2024-01-30 03:34:51 -0500

    fix header styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,5 @@
 <header class="sticky top-0 z-50 bg-blue-500 p-1 flex items-center justify-center">
-  <span class="text-sm absolute ml-2 left-4 top-1/2 transform -translate-y-1/2 text-white opacity-75">pre-pre-alpha</span>
+  <span class="text-sm absolute left-4 top-1/2 transform -translate-y-1/2 text-white opacity-75 hidden md:block">pre-pre-alpha</span>
   
   <nav class="flex justify-center">
     <a href="/" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-3">
@@ -19,7 +19,7 @@
     </a>
   </nav>
 
-  <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center">
-    <img src="/Twitch.svg" alt="Twitch" class="h-5 w-5 mr-2 no-modal"> <!-- Added class 'no-modal' -->
+  <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="absolute right-4 top-1/2 transform -translate-y-1/2 items-center hidden md:block">
+    <img src="/Twitch.svg" alt="Twitch" class="h-5 w-5 mr-2"> <!-- Adjust image size as needed -->
   </a>
 </header>
