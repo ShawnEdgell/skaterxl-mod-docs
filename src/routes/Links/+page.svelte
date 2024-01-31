80aa08593d1c33b93a4e4a9b8723348ed8ab0941
shawn edgell <shawn_edgell@hotmail.com>
2024-01-31 07:50:53 -0500

    links styling

--- src/routes/Links/+page.svelte
+++ src/routes/Links/+page.svelte
@@ -35,81 +35,60 @@
       <i>Similar to the SXL Modding Discord, Milky's Barnyard is a good source of information for modding Skater XL. Here you will find links to a variety of presets and settings by Milky and other community members.</i>
     </p>
 
-    <h2>Modder Discord Servers</h2>
-    <p>
-      Join the Modder Discords to connect directly with the creative minds behind your favorite Skater XL mods. Get the latest mod news and maybe even find hidden gems in special supporter sections.
-    </p>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/GmuBn6UzUh " target="_blank" rel="noopener noreferrer">DawgVinci's Discord</a>
-      <p>
-        <a class="text-blue-600" href="https://discord.com/invite/6575k57jbk " target="_blank" rel="noopener noreferrer">Fro's Discord</a>
-        <p>
-          <a class="text-blue-600" href="https://discord.com/invite/bYK5w53Jg6 " target="_blank" rel="noopener noreferrer">Guru's Discord</a>
+    <div class="flex flex-wrap -mx-2">
+      <!-- Left Column for Gear and Modder Discord Servers -->
+      <div class="w-full md:w-1/2 px-2">
 
-    <h2>Gear Discord Servers</h2>
-    <p>
-      Access custom XLGM gear and uniquely style your Skater XL experience.
-    </p>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/XHfcp258rv " target="_blank" rel="noopener noreferrer">Speeskeeks Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/cUnwY8ZZej " target="_blank" rel="noopener noreferrer">Sungodpxllz's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/47pyuCNuxG " target="_blank" rel="noopener noreferrer">Makes's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/pHQWsYG5Mm " target="_blank" rel="noopener noreferrer">Antikz's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/WaAKnSw6Ha " target="_blank" rel="noopener noreferrer">Hanzo's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/xQMXhCKfQ2 " target="_blank" rel="noopener noreferrer">CDT's (ClutchDidThat) Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/dpXEKGSVxw " target="_blank" rel="noopener noreferrer">Merlon Warlock's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/9EzS82SNqG " target="_blank" rel="noopener noreferrer">Stevyy's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/EUkY43zSWd " target="_blank" rel="noopener noreferrer">Kurbdogg420's Discord</a>
-    <p>
+    <!-- Modder Discord Servers Section -->
+    <div class="bg-blue-100 p-4 rounded-lg mb-4">
+      <h2 class="text-xl font-bold mt-2 mb-3">Modder Discord Servers</h2>
+      <p>Join the Modder Discords to connect directly with the creative minds behind your favorite Skater XL mods.</p>
+      <p><a href="https://discord.com/invite/GmuBn6UzUh" target="_blank" rel="noopener noreferrer">DawgVinci's Discord</a></p>
+      <p><a href="https://discord.com/invite/6575k57jbk" target="_blank" rel="noopener noreferrer">Fro's Discord</a></p>
+      <p><a href="https://discord.com/invite/bYK5w53Jg6" target="_blank" rel="noopener noreferrer">Guru's Discord</a></p>
+    </div>
 
-    <h2>Mapper Discord Servers</h2>
-    <p>
-      Mapper Discords are your gateway to exploring new terrains in Skater XL.
-    </p>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/A6Xm7jzYgT " target="_blank" rel="noopener noreferrer">Koannyc's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/YfadSmDM5J " target="_blank" rel="noopener noreferrer">Doog's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/b9Dv9atMtZ " target="_blank" rel="noopener noreferrer">Brookzee99's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/FfF6aFs4MX " target="_blank" rel="noopener noreferrer">Aaronb543's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/CnvsyAZ3Dy " target="_blank" rel="noopener noreferrer">ElixirDubstep's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/NaaSsB3z2y " target="_blank" rel="noopener noreferrer">DH13's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/PZrYPwBW8P " target="_blank" rel="noopener noreferrer">Jean-Olive's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/RZbYQjxHdW " target="_blank" rel="noopener noreferrer">Bralunits' Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/DVcJEhPh8M " target="_blank" rel="noopener noreferrer">B-row580's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/RuyygKEZzy " target="_blank" rel="noopener noreferrer">S4shko's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/q3ScY7V8rr " target="_blank" rel="noopener noreferrer">ValidusTM's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/H5r9PU6zj7 " target="_blank" rel="noopener noreferrer">Whynot_J's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/5WsUkXE5Xd " target="_blank" rel="noopener noreferrer">HeadShopper's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/qkvAMhrswY " target="_blank" rel="noopener noreferrer">BigCat's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/5RDe5PN3W3 " target="_blank" rel="noopener noreferrer">GuineaTiggs's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/EqYtcMYM9b " target="_blank" rel="noopener noreferrer">Blxcklock's Discord</a>
-    <p>
-      <a class="text-blue-600" href="https://discord.com/invite/s6fbmCx7 " target="_blank" rel="noopener noreferrer">Alexlaskka's Discord</a>
-    <p>
-  
+    <!-- Gear Discord Servers Section -->
+    <div class="bg-blue-100 p-4 rounded-lg mb-4">
+      <h2 class="text-xl font-bold mt-2 mb-3">Gear Discord Servers</h2>
+    <p>Access custom XLGM gear and uniquely style your Skater XL experience.</p>
+    <p><a href="https://discord.com/invite/XHfcp258rv " target="_blank" rel="noopener noreferrer">Speeskeeks Discord</a></p>
+    <p><a href="https://discord.com/invite/cUnwY8ZZej " target="_blank" rel="noopener noreferrer">Sungodpxllz's Discord</a></p>
+    <p><a href="https://discord.com/invite/47pyuCNuxG " target="_blank" rel="noopener noreferrer">Makes's Discord</a></p>
+    <p><a href="https://discord.com/invite/pHQWsYG5Mm " target="_blank" rel="noopener noreferrer">Antikz's Discord</a></p>
+    <p><a href="https://discord.com/invite/WaAKnSw6Ha " target="_blank" rel="noopener noreferrer">Hanzo's Discord</a></p>
+    <p><a href="https://discord.com/invite/xQMXhCKfQ2 " target="_blank" rel="noopener noreferrer">CDT's (ClutchDidThat) Discord</a></p>
+    <p><a href="https://discord.com/invite/dpXEKGSVxw " target="_blank" rel="noopener noreferrer">Merlon Warlock's Discord</a></p>
+    <p><a href="https://discord.com/invite/9EzS82SNqG " target="_blank" rel="noopener noreferrer">Stevyy's Discord</a></p>
+    <p><a href="https://discord.com/invite/EUkY43zSWd " target="_blank" rel="noopener noreferrer">Kurbdogg420's Discord</a></p>
+      </div>
+    </div>
+
+    <!-- Right Column for Mapper Discord Servers -->
+    <div class="w-full md:w-1/2 px-2">
+      <div class="bg-blue-100 p-4 rounded-lg mb-4">
+        <h2 class="text-xl font-bold mt-2 mb-3">Mapper Discord Servers</h2>
+        <p>Mapper Discords are your gateway to exploring new terrains in Skater XL.</p>
+    <p><a href="https://discord.com/invite/A6Xm7jzYgT " target="_blank" rel="noopener noreferrer">Koannyc's Discord</a></p>
+    <p><a href="https://discord.com/invite/YfadSmDM5J " target="_blank" rel="noopener noreferrer">Doog's Discord</a></p>
+    <p><a href="https://discord.com/invite/b9Dv9atMtZ " target="_blank" rel="noopener noreferrer">Brookzee99's Discord</a></p>
+    <p><a href="https://discord.com/invite/FfF6aFs4MX " target="_blank" rel="noopener noreferrer">Aaronb543's Discord</a></p>
+    <p><a href="https://discord.com/invite/CnvsyAZ3Dy " target="_blank" rel="noopener noreferrer">ElixirDubstep's Discord</a></p>
+    <p><a href="https://discord.com/invite/NaaSsB3z2y " target="_blank" rel="noopener noreferrer">DH13's Discord</a></p>
+    <p><a href="https://discord.com/invite/PZrYPwBW8P " target="_blank" rel="noopener noreferrer">Jean-Olive's Discord</a></p>
+    <p><a href="https://discord.com/invite/RZbYQjxHdW " target="_blank" rel="noopener noreferrer">Bralunits' Discord</a></p>
+    <p><a href="https://discord.com/invite/DVcJEhPh8M " target="_blank" rel="noopener noreferrer">B-row580's Discord</a></p>
+    <p><a href="https://discord.com/invite/RuyygKEZzy " target="_blank" rel="noopener noreferrer">S4shko's Discord</a></p>
+    <p><a href="https://discord.com/invite/q3ScY7V8rr " target="_blank" rel="noopener noreferrer">ValidusTM's Discord</a></p>
+    <p><a href="https://discord.com/invite/H5r9PU6zj7 " target="_blank" rel="noopener noreferrer">Whynot_J's Discord</a></p>
+    <p><a href="https://discord.com/invite/5WsUkXE5Xd " target="_blank" rel="noopener noreferrer">HeadShopper's Discord</a></p>
+    <p><a href="https://discord.com/invite/qkvAMhrswY " target="_blank" rel="noopener noreferrer">BigCat's Discord</a></p>
+    <p><a href="https://discord.com/invite/5RDe5PN3W3 " target="_blank" rel="noopener noreferrer">GuineaTiggs's Discord</a></p>
+    <p><a href="https://discord.com/invite/EqYtcMYM9b " target="_blank" rel="noopener noreferrer">Blxcklock's Discord</a></p>
+    <p><a href="https://discord.com/invite/s6fbmCx7 " target="_blank" rel="noopener noreferrer">Alexlaskka's Discord</a></p>
+    </div>
+    </div>
+  </div>
 
     <!-- More content here if needed -->
   </div>
