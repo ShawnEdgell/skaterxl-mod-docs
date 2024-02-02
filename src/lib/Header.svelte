3033dc9f267a17f68feb295b6e61c34cc189f5c1
shawn edgell <shawn_edgell@hotmail.com>
2024-02-02 02:29:04 -0500

    fixed header on mobile

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,17 +1,17 @@
-<header class="sticky top-0 z-50 bg-slate-900 p-4 flex items-center justify-center">
-  <span class="text-sm absolute left-4 top-1/2 transform -translate-y-1/2 text-white opacity-75 hidden md:block">pre-pre-alpha</span>
+<header class="sticky top-0 z-50 bg-slate-900 p-2 md:p-4 flex items-center justify-center relative">
+  <span class="text-xs md:text-sm left-4 top-1/2 -translate-y-1/2 text-white opacity-75 hidden md:hidden lg:block absolute">pre-pre-alpha</span>
   
-  <nav class="flex justify-center">
+  <nav class="text-xs sm:text-base">
     <!-- Navigation Links -->
-    <a href="/" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-6">Home</a>
-    <a href="/GettingStarted" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-6">Getting Started</a>
-    <a href="/Maps&Gear" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-6">Maps & Gear</a>
-    <a href="/Links" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-6">Links</a>
-    <a href="/News" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-6">News</a>
+    <a href="/" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2 sm:mx-6">Home</a>
+    <a href="/GettingStarted" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2 sm:mx-6">Getting Started</a>
+    <a href="/Maps&Gear" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2 sm:mx-6">Maps & Gear</a>
+    <a href="/Links" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2 sm:mx-6">Links</a>
+    <a href="/News" sveltekit-prefetch class="font-bold text-white hover:text-gray-300 mx-2 sm:mx-6">News</a>
   </nav>
 
   <!-- Twitch Link -->
-  <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="absolute right-4 top-1/2 transform -translate-y-1/2 items-center hidden md:block">
-    <img src="/Twitch.svg" alt="Twitch" class="h-5 w-5 mr-2">
+  <a href="https://www.twitch.tv/directory/game/Skater%20XL" target="_blank" rel="noopener noreferrer" class="h-5 w-5 absolute right-4 top-1/2 -translate-y-1/2 hidden md:hidden lg:block">
+    <img src="/Twitch.svg" alt="Twitch">
   </a>
 </header>
