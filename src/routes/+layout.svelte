1d3367591f5cb4b69db3dd3dc8bb56db63f21bb5
shawn edgell <shawn_edgell@hotmail.com>
2024-02-04 18:00:41 -0500

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,11 @@
   import Sidebar from '$lib/Sidebar.svelte';
   import '../app.css';
   import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+ 
+  inject({ mode: dev ? 'development' : 'production' });
+
 
   let isSidebarVisible = true; // Default to true for SSR or initial load
   let modal, modalImg;
