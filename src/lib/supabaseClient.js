67e81aa89e9150a675b84cb194afcdcc18536543
shawn edgell <shawn_edgell@hotmail.com>
2024-02-18 05:38:13 -0500

    fixed supabase link

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,9 @@
+// supabaseClient.js
+
 import { createClient } from '@supabase/supabase-js';
 
 // Access environment variables
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
