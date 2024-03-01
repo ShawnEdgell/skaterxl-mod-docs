024e7fe717c117cd494ce54462e721f21c131e95
shawn edgell <shawn_edgell@hotmail.com>
2024-03-01 07:53:18 -0500

    fixed image sizing

--- src/routes/xlhub/+page.svelte
+++ src/routes/xlhub/+page.svelte
@@ -5,7 +5,7 @@
 <div use:tocCrawler={{ mode: 'generate', scrollTarget: '#page' }} class="py-6 space-y-8">
 	<div class="space-y-5">
 		<h1 class="h1">XL Hub</h1>
-		<h2 class="h2">Skater XL Modding Made Easy</h2>
+		<h2 data-toc-ignore class="h2">Skater XL Modding Made Easy</h2>
 		<p>
 			Introducing XL Hub by Fro, the ultimate tool for browsing, downloading, and managing your
 			Skater XL maps and mods. No UnityModManager required.
@@ -15,14 +15,19 @@
 		>
 	</div>
 	<div class="space-y-5">
-		<h2 class="h2">Quick Start Guide</h2>
+		<h2 data-toc-ignore class="h2">Quick Start Guide</h2>
 		<p>
 			Download and open XL Hub. Log in with your mod.io account to access a vast range of Skater XL
 			mods. XL Hub detects your Skater XL installation via Steam, simplifying setup. Direct map
 			downloads to Documents/SkaterXL/Maps for easy access.
 		</p>
-		<div class="card p-4">
-			<img src="/images/XLhub.webp" alt="XL Hub Quick Start Guide" />
+		<div class="grid md:grid-cols-2 gap-5">
+			<div class="card p-4">
+				<img src="/images/XLHub2.webp" alt="XL Hub 2" />
+			</div>
+			<div class="card p-4">
+				<img src="/images/XLhub.webp" alt="XL Hub 1" />
+			</div>
 		</div>
 	</div>
 </div>
