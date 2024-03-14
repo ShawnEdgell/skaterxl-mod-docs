1c015b660541c68f4274ac97c994e07836945183
shawn edgell <shawn_edgell@hotmail.com>
2024-03-14 05:14:32 -0400

    removed auth

--- src/lib/components/layout/Navigation.svelte
+++ src/lib/components/layout/Navigation.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
-	import AuthButton from '$lib/components/ui/AuthButton.svelte';
 </script>
 
 <!-- Navigation Bar-->
 <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
 	<svelte:fragment slot="lead">
-		<AuthButton />
+		<div></div>
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
 		<LightSwitch />
--- src/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// src/lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // Ensure this is defined in your .env file
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // Ensure this is defined in your .env file
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
